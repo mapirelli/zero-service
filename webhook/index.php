0d734fbb4b60be5cffb926dd427110016e8aed8c
Mariano Pirelli <mariano.pirelli@dos-group.com>
2023-04-29 01:13:30 -0500

    zero-service 1.0.2 improving code and functions

--- webhook/index.php
+++ webhook/index.php
@@ -1,70 +1,95 @@
 <?php
-require_once __DIR__ . '/../functions.php';
+//require_once __DIR__ . '/../functions.php';
 /**
  * Webhook
  */
 $socketAddress = "tcp://*:5560";
+$authToken = "auth_token";
 $remoteAddress = $_SERVER['REMOTE_ADDR'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
-$channel = "XAUUSD.r";
 
-if ($requestMethod === 'POST') {
-    $alertPayload = file_get_contents('php://input');
-    $client = new Client($socketAddress);
-    if ($client->isValidPayload($alertPayload)) {
-        $client->publishAlert($channel, $alertPayload);
-    }
+if ($requestMethod === 'POST' ) {
+    $payload = json_decode(file_get_contents('php://input'), true);
+    $client = new Publisher($socketAddress, $authToken);
+    $client->publish($payload);
 }
 
 if ($requestMethod === 'GET') {
-    echo("Hello world");
-    if ($_GET && isset($_GET['alert'])) {
-        $alert = $_GET['alert'];
-        if ($alert == 'buy' || $alert == 'sell') {
-            $alertTester = new AlertTester();
-            $alertPayload  = $alertTester->getPayload($alert);
-            $client = new Client($socketAddress);
-            if ($client->isValidPayload($alertPayload)) {
-                $client->publishAlert($channel, $alertPayload);
-            }
-        }
+    echo("Up and running");
+    $testReq = new TestRequest();
+    if ($testReq->isValid()) {
+        $alertTest = new AlertTester($authToken);
+        $payload = $alertTest->getPayload($testReq->getSymbol(), $testReq->getAction(), $testReq->getContracts());
+        $client = new Publisher($socketAddress, $authToken);
+        if ($client->publish($payload))
+            echo(" & published");
     }
 }
 
-class Client {
+class Publisher {
     
     private $socketAddress;
 
-    private $identity;
+    private $authToken;
 
     static $messageLifeTime = 30000; //(-1 forever, 0 right now, > 0 expiration time) in milliseconds   
 
-    public function __construct(string $socketAddress, string $identity = 'webhook')
+    public function __construct(string $socketAddress, string $authToken)
     {
         $this->socketAddress = $socketAddress;
-        $this->identity = $identity;
+        $this->authToken = $authToken;
     }
 
-    public function isValidPayload(string $payload) {
+    protected function isValidPayload(array $payload): bool {
+        
+        $valid = (
+            isset($payload['auth_token']) &&
+            isset($payload['alert_time']) &&
+            isset($payload['alert_ticker']) &&
+            isset($payload['strategy_order_action']) &&
+            isset($payload['strategy_order_price']) &&
+            isset($payload['strategy_order_contracts'])
+        );
+
+        error_log('Processing payload...');
+        error_log(print_r($payload, true));
+
+        if ($valid == FALSE) {
+            error_log('Invalid payload');
+            return FALSE;
+        }
+        error_log('Valid payload');
         return TRUE;
     }
 
-    public function publishAlert(string $channel, string $payload) {
+    protected function isAuthorized(array $payload): bool {
+        if ($this->authToken == $payload['auth_token'])
+            return TRUE;
 
-        $context = new ZMQContext();
-        $socket = new ZMQSocket($context, ZMQ::SOCKET_PUB);
-        //$socket->setSockOpt(ZMQ::SOCKOPT_IDENTITY, $this->identity); not valid for SOCKET_PUB
-        //$socket->setSockOpt(ZMQ::SOCKOPT_LINGER, self::$messageLifeTime);
-        $socket->bind($this->socketAddress);
-        //send message
-        usleep (500000);
-        $socket->send($channel, ZMQ::MODE_SNDMORE);
-        $socket->send($payload);
-        usleep (1000);
-
+        error_log('Not authorized token: ' . $payload['auth_token']);
+        return FALSE;
     }
 
-    public function sendAlert(string $sender, string $payload) {
+    public function publish(array $payload): bool {
+
+        if ($this->isValidPayload($payload) && $this->isAuthorized($payload)) {
+            $context = new ZMQContext();
+            $socket = new ZMQSocket($context, ZMQ::SOCKET_PUB);
+            //$socket->setSockOpt(ZMQ::SOCKOPT_LINGER, self::$messageLifeTime);
+            $socket->bind($this->socketAddress);
+            //send message
+            usleep (500000);
+            $symbol = $payload['alert_ticker'];
+            $socket->send($symbol, ZMQ::MODE_SNDMORE);
+            $socket->send(json_encode($payload));
+            usleep (10000);
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+    /* abandoned code
+    public function sendAlert(string $payload) {
         $context = new ZMQContext();
         $socket = new ZMQSocket($context, ZMQ::SOCKET_DEALER);
         $socket->setSockOpt(ZMQ::SOCKOPT_IDENTITY, $this->identity);
@@ -73,43 +98,89 @@ class Client {
         $socket->send($payload);
         usleep(1000); //1000 microsecondi = 1 millisecondo //we let time to send the message
     }
+    */
 }
 
 class AlertTester {
 
-    protected $ticker;
+    private $authToken;
 
-    protected $timeframe;
+    private $exchange = 'TESTER';
 
-    static $exchange = 'TEST';
-
-    public function __construct(string $ticker = 'XAUUSD', string $timeframe = '1')
+    public function __construct(string $authToken)
     {
-        $this->ticker = $ticker;
-        $this->timeframe = $timeframe;
+        $this->authToken = $authToken;
     }
 
-    public function getPayload(string $action = 'buy', int $price = 0, int $size = 1) {
-        $timenow = date('Y-m-dTH:i:sZ');
-        $action == 'buy';
+    public function getPayload(string $symbol, string $action, int $contracts): array {
+        $price = 0;
+        $timeframe = '30M';
+        $time = date('Y-m-dTH:i:sZ');
         $position = 'long';
         $previus_position = 'sell';
+        $size = $contracts * 2;
         if ($action == 'sell') {
             $position = 'short';
             $previus_position = 'buy';
+            $size = $size * -1;
         }
-        $payload = [
-            'timenow' => $timenow,
-            'exchange' => self::$exchange,
-            'ticker' => $this->ticker,
-            'timeframe' => $this->timeframe,
+        return [
+            'auth_token' => $this->authToken,
+            'alert_name' => 'Test payload',
+            'alert_time' => $time,
+            'alert_exchange' => $this->exchange,
+            'alert_ticker' => $symbol,
+            'alert_timeframe' => $timeframe,
+            'candle_time' => $time,
+            'candle_close' => $price,
+            'candle_volume' => 100,
             'strategy_order_action' => $action,
+            'strategy_order_price' => $price,
+            "strategy_order_contracts" => $contracts,
+            'strategy_order_tp' => 6, //value %
+            'strategy_order_sl' => 2, //value %
             'strategy_market_position' => $position,
             'strategy_position_size' => $size,
-            'strategy_order_price' => $price,
             'strategy_prev_market_position' => $previus_position,
-            'strategy_prev_market_position_size' => $size,
+            'strategy_prev_market_position_size' => $size * -1,
         ];
-        return json_encode($payload);
+    }
+}
+
+
+
+class TestRequest {
+    
+    private $action;
+
+    private $symbol;
+
+    private $contracts;
+
+    public function isValid() {
+        if ($_GET && isset($_GET['action']) && isset($_GET['symbol'])) {
+            if (in_array($_GET['action'], ['buy','sell'])) {
+                $this->action = $_GET['action'];
+                $this->symbol = $_GET['symbol'];
+                $this->contracts = isset($_GET['contracts']) ? $_GET['contracts'] : 1;
+                return TRUE;
+            }
+        }
+        return FALSE;
+    }
+
+    public function getSymbol()
+    {
+        return $this->symbol;
+    }
+
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    public function getContracts()
+    {
+        return $this->contracts;
     }
 }
